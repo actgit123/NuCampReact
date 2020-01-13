9449e07414437e43030881b955b8ae490e300817
actgit123 <pavaniv@outlook.com>
2020-01-12 22:48:59 -0800

    forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,11 +5,13 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';    
 import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
 import { PROMOTIONS } from '../shared/promotions';
+
 class Main extends Component {
     constructor(props) {
         super(props);
@@ -44,10 +46,11 @@ class Main extends Component {
             <div>
                  <Header/> 
                  <Switch>  
-                     <Route path='/directory/:campsiteId' component={CampsiteWithId} /> 
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} /> 
                     <Route path = '/home' component = {HomePage}/>  
                     <Route exact path = '/directory' render= {() =><Directory campsites={this.state.campsites} />} />
                     <Route exact path='/contactus' component={Contact}/>
+                    <Route exact path='/aboutus' render={() => <About partners ={this.state.partners}/>}/>
                     <Redirect to='/home' />
                 </Switch>               
                 <Footer />
